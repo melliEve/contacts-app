f36a1f986517581514fb548b05c038c50cbb2242
melli <melsie78@hotmail.com>
2019-10-01 11:48:04 +0200

    create server

---
+++ index.js
@@ -0,0 +1,16 @@
+// Require the express module
+const express = require('express');
+// Create a new web server
+const app = express();
+// Tell the web server to serve files
+// from the www folder
+app.use(express.static('www'));
+// Start the web server on port 3000
+app.listen(3000,() => console.log('Listening on port 3000'));
+ 
+// Serve the index page everywhere so that the
+// frontend router can decide what to do
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './www/index.html'));
+});
